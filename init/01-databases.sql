63823f60bfe0885455023dc2af19d0b08b9bd269
Nicolas <nicolascleiton700e@gmail.com>
2022-12-18 02:21:01 -0300

    fix docker compose

--- init/01-databases.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `notification_service`;
-CREATE DATABASE IF NOT EXISTS `shadow_notification_service`;
-
-GRANT ALL ON *.* TO 'root'@'localhost'; 
--- src/infra/db/prisma/prisma.service.ts
+++ src/infra/db/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaClientInitializationError } from '@prisma/client/runtime';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
@@ -10,7 +11,22 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   // }
 
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (err) {
+      if (
+        err instanceof PrismaClientInitializationError &&
+        err.name === "Can't reach database server at"
+      ) {
+        console.warn(
+          'cannot reach the actual database, trying to cannot in the next 30 seconds',
+        );
+        setTimeout(async () => {
+          await this.$connect();
+          console.log('connection was successfuly');
+        }, 30000);
+      }
+    }
   }
 
   async enableShutdownHooks(app: INestApplication) {
